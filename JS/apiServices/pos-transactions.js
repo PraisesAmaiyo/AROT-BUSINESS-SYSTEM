c80ee19217839d69bcd3e5d2c28830880b389ece
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-11-03 07:56:39 +0100

    MOD

--- JS/apiServices/pos-transactions.js
+++ JS/apiServices/pos-transactions.js
@@ -3,6 +3,9 @@ import config from '../../config.js';
 const baseUrl = config.baseUrl;
 const apiToken = config.token;
 
+console.log(baseUrl);
+console.log(apiToken);
+
 export async function getPosTransactions() {
   try {
     //  console.log('Sending GET request...');
